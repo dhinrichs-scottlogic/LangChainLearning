97881691436382696a5eee94b1e91475ab458110
dhinrichs-scottlogic <125262707+dhinrichs-scottlogic@users.noreply.github.com>
2024-05-23 09:27:21 +0100

    added text splitter

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
-
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
@@ -11,21 +11,33 @@ llm = ChatOpenAI(
 )
 
 # single prompt
-response = llm.invoke("Hello, how are you?")
-print(response)
+# response = llm.invoke("Hello, how are you?")
+# print(response)
 
 # multiple prompts
-response = llm.batch(["Hello, how are you?", "Write a poem about AI"])
-print(response)
+# response = llm.batch(["Hello, how are you?", "Write a poem about AI"])
+# print(response)
 
 # receive response in chunks
-response = llm.stream("Write a poem about AI")
-for chunk in response:
-    print(chunk.content, end="", flush=True)
+# response = llm.stream("Write a poem about AI")
+# for chunk in response:
+    # print(chunk.content, end="", flush=True)
 
 
 # load text files from a directory
 loader = DirectoryLoader('./data', glob="**/*.md", loader_cls=TextLoader)
 documents = loader.load()
-print(documents[0].page_content[:100])
+print(len(documents))
+# print the first 100 characters from the first document
+# print(documents[0].page_content[:100])
 
+# split loaded text into chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=50,
+    length_function=len,
+    add_start_index=True,
+)
+
+chunks = text_splitter.split_documents(documents)
+print(len(chunks))
---
+++ data/lyrics.md
@@ -0,0 +1,24 @@
+Well, if you want to sing out, sing out
+And if you want to be free, be free
+'Cause there's a million things to be
+You know that there are
+
+And if you want to live high, live high
+And if you want to live low, live low
+'Cause there's a million ways to go
+You know that there are
+
+You can do what you want, the opportunity's on
+And if you find a new way, you can do it today
+You can make it all true and you can make it undo
+You see, it's easy, you only need to know
+
+Well, if you want to say yes, say yes
+And if you want to say no, say no
+'Cause there's a million ways to go
+You know that there are
+
+And if you want to be me, be me
+And if you want to be you, be you
+'Cause there's a million things to do
+You know that there are
